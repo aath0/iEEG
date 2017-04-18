9364dc110463f3db901794a413191d4f9262a8c3
Athina Tzovara <athina.tz@gmail.com>
2017-04-18 17:55:34 +0200

    Add files via upload
    Computes spatial clustering of contacts, according to their position within a stripe. 
    Tested for stripes of 7 contacts.

---
+++ prepare_neighboors_intracranial.m
@@ -0,0 +1,54 @@
+p_id = 'MC';
+
+filep_g = 'D:\Data\old_studies\Intracranial_GMMN\'
+filep = [filep_g, p_id, '\models\'];
+
+bb = 1;
+%load pre-processed data:
+load([filep,'iEEG_bip_block', num2str(bb),'_f.mat'])
+
+nchan = length(iEEGf.label);
+strip_length = 7; %after bipolar ref
+connectivity = zeros(nchan,nchan);
+
+for ch = 1:nchan
+    
+    neighbours(ch).label = iEEGf.label{ch};
+    
+    if mod(ch,strip_length) == 0 %end of stripe
+        neighbours(ch).neighblabel = {iEEGf.label(ch-1) iEEGf.label(ch-2)};
+        connectivity(ch,[ch-1 ch-1]) = 1;
+        
+    else if mod(ch,strip_length) == 1 %beginning for stripe
+            neighbours(ch).neighblabel = {iEEGf.label(ch+1) iEEGf.label(ch+2)};
+            connectivity(ch,[ch+1 ch+2]) = 1;
+            
+        else %all the other cases:
+            if mod(ch,strip_length) == 2
+                
+                neighbours(ch).neighblabel = {iEEGf.label(ch-1) iEEGf.label(ch+1) iEEGf.label(ch+2)};
+                connectivity(ch,[ch-1]) = 1;
+                connectivity(ch,[ch+2 ch+1]) = 1;
+                
+            else
+                if mod(ch,strip_length) == 6
+                    
+                    neighbours(ch).neighblabel = { iEEGf.label(ch-2) iEEGf.label(ch-1) iEEGf.label(ch+1)};
+                    connectivity(ch,[ch+1]) = 1;
+                    connectivity(ch,[ch-2 ch-1]) = 1;
+                    
+                else
+                    neighbours(ch).neighblabel = {iEEGf.label(ch-1) iEEGf.label(ch-2) iEEGf.label(ch+1) iEEGf.label(ch+2)};
+                    connectivity(ch,[ch-2 ch-1]) = 1;
+                    connectivity(ch,[ch+2 ch+1]) = 1;
+                end
+            end
+        end
+    end
+end
+
+save([filep,'channels_neighbours.mat'],'neighbours','connectivity')
+
+
+
+
